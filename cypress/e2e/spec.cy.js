8da1ae49082855519163320642309af094bc0e51
Gerfson Alves <gerfson17@hotmail.com>
2025-03-12 15:25:43 -0300

    Spec

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,9 @@
-describe('template spec', () => {
-  it('Site', () => {
-    cy.visit('https://www.itau.com.br/')
-  })
-})
+describe('OrangeHRM - Login', () => {
+  it('Deve fazer login com sucesso', () => {
+    cy.visit('https://opensource-demo.orangehrmlive.com/');
+    cy.get('input[name="username"]').type('Admin');
+    cy.get('input[name="password"]').type('admin123');
+    cy.get('button[type="submit"]').click();
+    cy.contains('Dashboard').should('be.visible');
+  });
+});
